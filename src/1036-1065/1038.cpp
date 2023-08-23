98a5ec5d17c8894f17efb088ec4d8c6de551ff3e
romeoahmed <ahmedorqwn@gmail.com>
2023-08-23 16:09:17 +0800

    Update some cpp files

---
+++ src/1036-1065/1038.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Romeo Ahmed <ahmedorqwn@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <map>
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char const *argv[])
+{
+    auto l = 0, h = 0;
+    std::map<int, double> temperature;
+    std::cin >> l >> h;
+
+    for (auto i = l; i < h; i++)
+    {
+        temperature[i] = (i - 32) * 5.0 / 9.0;
+    }
+
+    std::cout.setf(std::ios::right);
+    for (std::map<int, double>::const_iterator iter = temperature.begin(); 
+         iter != temperature.end(); ++iter)
+    {
+        std::cout << std::setw(8) << iter->first << std::setw(10) 
+                  << std::fixed << std::setprecision(2) << iter->second << std::endl;
+    }
+
+    return 0;
+}
